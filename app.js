16f1347a5fe4e18d34a6fbc1158cb5ceef944cb8
Jonathan King <jonathanlking@me.com>
2014-07-29 10:06:20 +0100

    JSON cycle data now works

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 var http = require('http');
+
 var request = require('request');
 var fileStream = require('fs');
 var parseString = require('xml2js').parseString;
@@ -7,27 +8,36 @@ http.createServer(function (request, responce) {
   responce.writeHead(200, {'Content-Type': 'text/plain'});
   
   cacheXML();
-  
-
-  
-/*
-  var request = http.get("http://www.tfl.gov.uk/tfl/syndication/feeds/cycle-hire/livecyclehireupdates.xml", function(response) {
- 
-	  
-  parseString(response, function (err, result) {
-    console.dir(result);
-	});
-  
+  cycleData(function(data){
+  	 // The data is provided in a closure, as it is read from file.
+  	 var lastUpdated = new Date(data.stations.$.lastUpdate);
+  	 responce.end();
   });
-*/
-  
-  responce.end('Hello World\n');
+
 }).listen(1337, '127.0.0.1');
 console.log('Server running at http://127.0.0.1:1337/');
 
+function cycleData(callback) {
+	
+	// Read the cached data
+	
+	fileStream.readFile('cache.xml', 'utf8', function (error,file) {
+		if (error) console.log(error);
+		else {
+			
+			// Read the cached data, convert it to json and return it.
+			
+			parseString(file, function (error, result) {
+				var json = JSON.stringify(result, null, 4);
+				callback(JSON.parse(json));
+			}); 
+			
+		}
+	});
+	
+}
 
-
-var cacheXML = function () {
+function cacheXML() {
     
     // Download the data from TFL every 3 minutes and save to file
     
@@ -36,37 +46,13 @@ var cacheXML = function () {
 			
 			// Cache the data to file
 			
-			fileStream.writeFile("cache.xml", body, function(error) {
+			fileStream.writeFile('cache.xml', body, function(error) {
 				if(error) console.log(error);
 				else console.log("The file was saved!");
-			}); 
+			});
 			
 		}
 	});
     
-/*
-    var cache = fileStream.createWriteStream("cache.xml");
-    var request = http.get("http://www.tfl.gov.uk/tfl/syndication/feeds/cycle-hire/livecyclehireupdates.xml", function(responce) {
-		responce.pipe(cache);
-		console.log(responce);
-		console.log("Download complete!");
-	});
-*/
-	
-/*
-	http.get("http://www.tfl.gov.uk/tfl/syndication/feeds/cycle-hire/livecyclehireupdates.xml", function(res) {
-		
-		console.log("Got response: " + res.statusCode);
-		
-		res.on("data", function(chunk) {
-			console.log("BODY: " + chunk);
-		});
-		}).on('error', function(e) {
-			console.log("Got error: " + e.message);
-	});
-*/
-	
-	
-	
     setTimeout(cacheXML,1000*60*3);
 }
