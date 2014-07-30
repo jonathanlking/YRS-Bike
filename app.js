264b119a47fd35fa6f0448bc6ef6b7b95c50be61
Jonathan King <jonathanlking@me.com>
2014-07-30 20:44:38 +0100

    Finally starting work on the actual app! Integrated express.

--- app.js
+++ app.js
@@ -1,60 +1,27 @@
-var http = require('http');
+var express = require('express');
+var app = express();
 
-var request = require('request');
-var fileStream = require('fs');
-var parseString = require('xml2js').parseString;
+var server = app.listen(3000, 'localhost', function() {
+    console.log('Listening on port %d', server.address().port);
+});
 
-http.createServer(function (request, responce) {
-  responce.writeHead(200, {'Content-Type': 'text/plain'});
-  
-  cacheXML();
-  cycleData(function(data){
-  	 // The data is provided in a closure, as it is read from file.
-  	 
-  	 console.log(data.stations.$.lastUpdate);
-  	 responce.end("Working fine."); 
-  });
+console.log('Server running.');
 
-}).listen(1337, '127.0.0.1');
-console.log('Server running at http://127.0.0.1:1337/');
+var error = 400;
 
-function cycleData(callback) {
-	
-	// Read the cached data
-	
-	fileStream.readFile('cache.xml', 'utf8', function (error,file) {
-		if (error) console.log(error);
-		else {
-			
-			// Read the cached data, convert it to json and return it.
-			
-			parseString(file, function (error, result) {
-				callback(result);
-			}); 
-			
-		}
-	});
-	
-}
+app.get('/route/', function(request, responce)
+{
 
-function cacheXML() {
-    
-    // Download the data from TFL every 3 minutes and save to file
-    
-	request('http://www.tfl.gov.uk/tfl/syndication/feeds/cycle-hire/livecyclehireupdates.xml', function (error, response, body) {
-		if (!error && response.statusCode == 200) {
-		
-			console.log("File downloaded.");
-			
-			// Cache the data to file
-			
-			fileStream.writeFile('cache.xml', body, function(error) {
-				if(error) console.log(error);
-				else console.log("File cached.");
-			});
-			
-		}
-	});
-    
-    setTimeout(cacheXML,1000*60*3);
-}
+	var from_latitude = request.param("from_latitude");
+	var from_longitude = request.param("from_longitude");
+	var to_latitude = request.param("to_latitude");
+	var to_longitude = request.param("to_longitude");
+
+	if (!from_latitude || !from_longitude || !to_latitude || !to_longitude) responce.send(error);
+
+	else
+	{
+		responce.send("Happy!");
+	}
+
+});
