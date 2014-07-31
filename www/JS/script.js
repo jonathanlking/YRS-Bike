ab44629c115e90f69773f66a331b990e9ef15abd
guava6 <Jess@Jessicas-MacBook-Pro.local>
2014-07-31 11:05:42 +0100

    Integrated website files

---
+++ www/JS/script.js
@@ -0,0 +1,16 @@
+$("#searchButton").click(function(){
+
+var jqxhr = $.get( "pedal.jlk.co", function() {
+  alert( "success" );
+})
+  .done(function() {
+    alert( "second success" );
+  })
+  .fail(function() {
+    alert( "error" );
+  })
+  .always(function() {
+    alert( "finished" );
+  });
+
+});
